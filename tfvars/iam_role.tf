14eb6b2ce2c2ff89c11a856fadc475eda95195ec
HariDEV-GIT <78740469+HariDEV-GIT@users.noreply.github.com>
2025-07-28 07:48:24 +0530

    Create iam_role.tf

---
+++ tfvars/iam_role.tf
@@ -0,0 +1,18 @@
+resource "aws_iam_role" "example_role" {
+  name               = "example_role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action    = "sts:AssumeRole"
+        Principal = {
+          Service = "ec2.amazonaws.com"  # Change as needed
+        }
+        Effect    = "Allow"
+        Sid       = ""
+      },
+    ]
+  })
+
+  tags = var.common_tags  # Use the centralized tags
+}
