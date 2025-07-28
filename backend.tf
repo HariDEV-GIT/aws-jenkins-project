149a5de464cd0153c46cf951aac7ba631e655d19
HariDEV-GIT <78740469+HariDEV-GIT@users.noreply.github.com>
2025-07-28 23:14:37 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "terraform-statefile-bucket-demo-project "
+    bucket         = "terraform-statefile-bucket-demo-project"
     key            = "terraform/state"
     region         = "us-east-1"
   }
