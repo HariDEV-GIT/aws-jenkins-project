b1362bd99cf1b2b053629292cf84162bce4b7236
HariDEV-GIT <78740469+HariDEV-GIT@users.noreply.github.com>
2025-07-28 08:20:52 +0530

    Create iam_policy_attachment.tf

---
+++ iam_policy_attachment.tf
@@ -0,0 +1,44 @@
+resource "aws_iam_role_policy_attachment" "role1_policy1" {
+  policy_arn = aws_iam_policy.example_policy_1.arn
+  role       = aws_iam_role.example_role_1.name
+}
+
+resource "aws_iam_role_policy_attachment" "role1_policy2" {
+  policy_arn = aws_iam_policy.example_policy_2.arn
+  role       = aws_iam_role.example_role_1.name
+}
+
+resource "aws_iam_role_policy_attachment" "role1_policy3" {
+  policy_arn = aws_iam_policy.example_policy_3.arn
+  role       = aws_iam_role.example_role_1.name
+}
+
+resource "aws_iam_role_policy_attachment" "role2_policy1" {
+  policy_arn = aws_iam_policy.example_policy_1.arn
+  role       = aws_iam_role.example_role_2.name
+}
+
+resource "aws_iam_role_policy_attachment" "role2_policy2" {
+  policy_arn = aws_iam_policy.example_policy_2.arn
+  role       = aws_iam_role.example_role_2.name
+}
+
+resource "aws_iam_role_policy_attachment" "role2_policy3" {
+  policy_arn = aws_iam_policy.example_policy_3.arn
+  role       = aws_iam_role.example_role_2.name
+}
+
+resource "aws_iam_role_policy_attachment" "role3_policy1" {
+  policy_arn = aws_iam_policy.example_policy_1.arn
+  role       = aws_iam_role.example_role_3.name
+}
+
+resource "aws_iam_role_policy_attachment" "role3_policy2" {
+  policy_arn = aws_iam_policy.example_policy_2.arn
+  role       = aws_iam_role.example_role_3.name
+}
+
+resource "aws_iam_role_policy_attachment" "role3_policy3" {
+  policy_arn = aws_iam_policy.example_policy_3.arn
+  role       = aws_iam_role.example_role_3.name
+}
