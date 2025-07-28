aacdff0b43dad432fce2eb17cc8d98456e7f9804
HariDEV-GIT <78740469+HariDEV-GIT@users.noreply.github.com>
2025-07-28 07:51:13 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,3 +1,29 @@
+variable "region" {
+  description = "The AWS region to deploy resources"
+  default     = "us-east-1"
+}
+
+variable "bucket_name" {
+  description = "The name of the S3 bucket"
+  type        = string
+}
+
+variable "db_username" {
+  description = "The username for the RDS instance"
+  type        = string
+}
+
+variable "db_password" {
+  description = "The password for the RDS instance"
+  type        = string
+  sensitive   = true
+}
+
+variable "db_name" {
+  description = "The name of the database"
+  type        = string
+}
+
 variable "dev_account_id" {
   description = "The AWS account ID for the development environment"
   type        = string
