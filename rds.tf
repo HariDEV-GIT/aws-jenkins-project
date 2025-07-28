17a1fa2614753245d0ec47b8849124587ab94f94
HariDEV-GIT <78740469+HariDEV-GIT@users.noreply.github.com>
2025-07-28 21:49:11 +0530

    Update rds.tf

--- rds.tf
+++ rds.tf
@@ -1,13 +1,14 @@
-resource "aws_db_instance" "example_rds" {
+/* resource "aws_db_instance" "example_rds" {
   identifier         = "example-rds"
-  engine             = "mysql"  # Change to your desired database engine
-  engine_version     = "8.0"     # Change as needed
-  instance_class     = "db.t2.micro"  # Change to your desired instance class
+  engine             = "mysql"  
+  engine_version     = "8.0"    
+  instance_class     = "db.t2.micro" 
   allocated_storage   = 20
-  username           = var.db_username  # Use the variable defined in variables.tf
-  password           = var.db_password  # Use the variable defined in variables.tf
-  db_name            = var.db_name  # Use the variable defined in variables.tf
+  username           = var.db_username  
+  password           = var.db_password  
+  db_name            = var.db_name 
   skip_final_snapshot = true
 
-  tags = var.common_tags  # Use the centralized tags
+  tags = var.common_tags 
 }
+*/
