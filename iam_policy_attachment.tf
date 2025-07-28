9990d825f331708e49c79744f4ebe9e35d605237
HariDEV-GIT <78740469+HariDEV-GIT@users.noreply.github.com>
2025-07-28 21:47:56 +0530

    Update iam_policy_attachment.tf

--- iam_policy_attachment.tf
+++ iam_policy_attachment.tf
@@ -1,9 +1,9 @@
-resource "aws_iam_role_policy_attachment" "role1_policy1" {
-  policy_arn = aws_iam_policy.example_policy_1.arn
-  role       = aws_iam_role.example_role_1.name
+resource "aws_iam_role_policy_attachment" "s3_policy_role" {
+  policy_arn = aws_iam_policy.s3_policy.arn
+  role       = aws_iam_role.s3_role.name
 }
 
-resource "aws_iam_role_policy_attachment" "role1_policy2" {
+// resource "aws_iam_role_policy_attachment" "role1_policy2" {
   policy_arn = aws_iam_policy.example_policy_2.arn
   role       = aws_iam_role.example_role_1.name
 }
@@ -37,7 +37,7 @@ resource "aws_iam_role_policy_attachment" "role3_policy2" {
   policy_arn = aws_iam_policy.example_policy_2.arn
   role       = aws_iam_role.example_role_3.name
 }
-
+//
 resource "aws_iam_role_policy_attachment" "role3_policy3" {
   policy_arn = aws_iam_policy.example_policy_3.arn
   role       = aws_iam_role.example_role_3.name
