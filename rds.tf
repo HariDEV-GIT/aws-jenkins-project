b410d690e8c31e3c54c6667b2adf3cd973a6e087
HariDEV-GIT <78740469+HariDEV-GIT@users.noreply.github.com>
2025-07-30 12:52:40 +0530

    Update rds.tf

--- rds.tf
+++ rds.tf
@@ -1,14 +1,43 @@
-/* resource "aws_db_instance" "example_rds" {
-  identifier         = "example-rds"
-  engine             = "mysql"  
-  engine_version     = "8.0"    
-  instance_class     = "db.t2.micro" 
-  allocated_storage   = 20
-  username           = var.db_username  
-  password           = var.db_password  
-  db_name            = var.db_name 
-  skip_final_snapshot = true
+// security group 
+resource "aws_security_group" "my_db_sg" {
+  name        = "rds_sg"
+  description = "Allow access to RDS instance"
+  vpc_id      = "vpc-eff25692"  
+  tags        = var.common_tags  
 
-  tags = var.common_tags 
+  ingress {
+    from_port   = 3306  # Change to your database port
+    to_port     = 3306  # Change to your database port
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # Change to your desired CIDR block for security
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
-*/
+
+// subnet group 
+resource "aws_db_subnet_group" "my_db_subnet_group" {
+  name       = "rds_db_subnet_group"
+  subnet_ids = ["subnet-079540737949d4ae9"]
+  tags       = var.common_tags  # Apply common tags if you have them defined
+}
+
+// RDS instance
+resource "aws_db_instance" "my_db" {
+  identifier               = "aws-demo-db"
+  engine                   = "mysql"  
+  engine_version           = "8.0"    
+  instance_class           = "db.t3.micro"  
+  allocated_storage        = 20  
+  db_subnet_group_name     = aws_db_subnet_group.my_db_subnet_group.name
+  vpc_security_group_ids   = [aws_security_group.my_db_sg.id]
+  username                 = var.db_username  
+  password                 = var.db_password  
+  skip_final_snapshot      = false 
+  tags                     = var.common_tags  
+}
