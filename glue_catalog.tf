085d159291833453b16ad771d84f8d035b215877
HariDEV-GIT <78740469+HariDEV-GIT@users.noreply.github.com>
2025-07-30 17:08:35 +0530

    Update glue_catalog.tf

--- glue_catalog.tf
+++ glue_catalog.tf
@@ -1,5 +1,5 @@
 resource "aws_glue_catalog_database" "glue_db" {
   name         = "glue_db_demo"
-  description  = "This is my Glue Database for data cataloging"
+  description  = "Glue DataDBbase for data cataloging"
   tags         = var.common_tags  
 }
