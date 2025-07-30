11cf31f9b18ea83445f39f262300ca22e24fad42
HariDEV-GIT <78740469+HariDEV-GIT@users.noreply.github.com>
2025-07-30 17:09:16 +0530

    Update iam_role.tf

--- iam_role.tf
+++ iam_role.tf
@@ -1,12 +1,12 @@
-resource "aws_iam_role" "s3_role" {
-  name               = "s3"
+resource "aws_iam_role" "s3" {
+  name               = "s3_role"
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
       {
         Action    = "sts:AssumeRole"
         Principal = {
-          Service = "ec2.amazonaws.com" 
+          Service = "s3.amazonaws.com" 
         }
         Effect    = "Allow"
         Sid       = ""
