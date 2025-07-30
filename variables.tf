37616bd35dcbe502dd407d290bf98ad5164f4b93
HariDEV-GIT <78740469+HariDEV-GIT@users.noreply.github.com>
2025-07-30 13:58:28 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,34 +1,34 @@
 variable "region" {
-  description = "The AWS region to deploy resources"
-  default     = "us-east-1"
+  description   = "The AWS region to deploy resources"
+  default       = "us-east-1"
 }
 
 variable "db_username" {
-  description = "The username for the rds database"
-  type        = string
+  description   = "The username for the rds database"
+  type          = "rdsadmin"
 }
 
 variable "db_password" {
-  description = "The password for the rds database"
-  type        = string
-  sensitive   = true
+  description   = "The password for the rds database"
+  type          = string
+  sensitive     = true
 }
 
 variable "dev_account_id" {
-  description    = "The AWS account ID for the development environment"
-  default        = "420602990842"
+  description   = "The AWS account ID for the development environment"
+  default       = "420602990842"
 }
 
 variable "prod_account_id" {
-  description    = "The AWS account ID for the production environment"
-  default        = "454673294031"
+  description   = "The AWS account ID for the production environment"
+  default       = "454673294031"
 }
 
 variable "common_tags" {
-  description = "Common tags for all resources"
-  type        = map(string)
-  default     = {
-    Environment = "Dev"  # Default to Dev, can be overridden in prod.tfvars
-    Project     = "YourProjectName"  # Change as needed
+  description   = "Common tags for all resources"
+  type          = map(string)
+  default       = {
+    Environment = "Dev" 
+    Project     = "YourProjectName" 
   }
 }
