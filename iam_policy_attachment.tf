7bb65b37835b577333f21e1e38e3197aa66e853e
HariDEV-GIT <78740469+HariDEV-GIT@users.noreply.github.com>
2025-07-31 10:35:31 +0530

    Update iam_policy_attachment.tf

--- iam_policy_attachment.tf
+++ iam_policy_attachment.tf
@@ -2,3 +2,10 @@ resource "aws_iam_role_policy_attachment" "policy_attach" {
   policy_arn = aws_iam_policy.policy.arn
   role       = aws_iam_role.s3.name
 }
+
+//backend deny
+resource "aws_iam_policy_attachment" "attach_deny_policy" {
+  name       = "attach_deny_policy"
+  policy_arn = aws_iam_policy.deny_s3_access.arn
+  roles      = [aws_iam_role.terraform_role.name]
+}
