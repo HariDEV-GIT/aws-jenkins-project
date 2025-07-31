2632b929ecab5d08537bcf35d65c99f566d75409
HariDEV-GIT <78740469+HariDEV-GIT@users.noreply.github.com>
2025-07-31 10:33:10 +0530

    Update iam_policy.tf

--- iam_policy.tf
+++ iam_policy.tf
@@ -1,3 +1,4 @@
+// S3 policy
 resource "aws_iam_policy" "policy" {
   name        = "s3_policy"
   description = "S3 policy for accessing resources"
@@ -12,6 +13,23 @@ resource "aws_iam_policy" "policy" {
       },
     ]
   })
+  tags = var.common_tags
+}
 
+// S3 deny policy
+resource "aws_iam_policy" "deny_s3_access" {
+  name        = "DenyS3Access"
+  description = "Deny all actions on the S3 bucket for Terraform state"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Deny"
+        Action = "s3:*"
+        Resource = "arn:aws:s3:::terraform-statefile-bucket-demo-project/*"
+      }
+    ]
+  })
   tags = var.common_tags
 }
