35564b737bc05781a861a206142369d01be366ea
HariDEV-GIT <78740469+HariDEV-GIT@users.noreply.github.com>
2025-07-31 06:45:18 +0530

    Delete backend directory

--- backend/dev/backend.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "terraform-statefile-bucket-demo-project"
-    key            = "terraform/state"
-    region         = "us-east-1"
-  }
-}
--- backend/prod/backend.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "terraform-statefile-bucket-demo-project-prod"
-    key            = "terraform/state"
-    region         = "ap-south-1"
-  }
-}
