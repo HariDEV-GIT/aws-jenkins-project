6135d548c05a84151ed8ac2df2209e09753484cd
HariDEV-GIT <78740469+HariDEV-GIT@users.noreply.github.com>
2025-07-31 10:34:52 +0530

    Update iam_role.tf

--- iam_role.tf
+++ iam_role.tf
@@ -13,6 +13,25 @@ resource "aws_iam_role" "s3" {
       },
     ]
   })
+  tags = var.common_tags
+}
 
+// S3 deny role
+resource "aws_iam_role" "terraform_role" {
+  name = "TerraformS3AccessRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Principal = {
+          Service = "s3.amazonaws.com"
+        }
+        Effect = "Allow"
+        Sid    = ""
+      }
+    ]
+  })
   tags = var.common_tags
 }
