d12ed34922646346792b1b6d9ae90fac8edfdb14
HariDEV-GIT <78740469+HariDEV-GIT@users.noreply.github.com>
2025-08-01 09:42:48 +0530

    Update secretsmanager.tf

--- secretsmanager.tf
+++ secretsmanager.tf
@@ -1,5 +1,5 @@
 resource "aws_secretsmanager_secret" "sm" {
-  name         = "demo_secret_tech"
+  name         = "demo_secret_techops"
   description  = "Secret for storing aws-jenkins information"
   tags         = var.common_tags 
 }
