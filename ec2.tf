c9af57855a875c25d8248683a93b07168b0bfa7a
HariDEV-GIT <78740469+HariDEV-GIT@users.noreply.github.com>
2025-08-01 09:47:47 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -2,6 +2,7 @@ resource "aws_instance" "ec2_instance" {
   ami           = "ami-020cba7c55df1f615" 
   instance_type = "t2.micro"
   key_name      = "jenkins_key"
+  subnet_id     = "subnet-079540737949d4ae9"
   tags = {
   Name = "aws-demo"
   } 
